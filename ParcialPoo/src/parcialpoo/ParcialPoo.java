1f8ff2efbfa4959526890a654e5b8d11ccd93acd
stephvm11 <00056024@uca.edu.sv>
2025-07-04 11:04:18 -0600

    Agregar archivo base

---
+++ ParcialPoo/src/parcialpoo/ParcialPoo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parcialpoo;
+
+/**
+ *
+ * @author steph
+ */
+public class ParcialPoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
